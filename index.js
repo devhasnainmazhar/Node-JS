d0ca3e16f88809ccc9f855989fce49e8c0486c42
Harry <hasnainmazhar43@gmail.com>
2022-09-01 20:39:09 +0500

    Day 1 and 6 videos completed

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+// fs.mkdirSync("hasnain");
+
+// fs.writeFileSync("./hasnain/bio.txt", "Hey this is Hasnain:)");
+
+// fs.appendFileSync("./hasnain/bio.txt", "\nI am here to help you.");
+
+// const org = fs.readFileSync("./hasnain/bio.txt").toString();
+// console.log(org);
+
+// fs.renameSync("./hasnain/bio.txt", "./hasnain/Mybio.txt");
+
+// fs.unlinkSync("./hasnain");
+// fs.unlinkSync("bio.txt");
